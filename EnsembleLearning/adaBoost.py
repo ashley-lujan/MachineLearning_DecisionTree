6dcb97ee96ee951699b53843a84628631677e31d
ashley-lujan <lujanashlet@gmail.com>
2024-10-16 20:35:16 -0600

    modified d3 to use a dataset that has a classs that maintains weight and the data values.

--- EnsembleLearning/adaBoost.py
+++ EnsembleLearning/adaBoost.py
@@ -1,4 +1,5 @@
 from weightedSet import WeightedX
+from weightedDecAlgo import DecisionTree
 
 #returns a list of weightedX's
 def saveDataSet(filename):
@@ -12,9 +13,32 @@ def saveDataSet(filename):
             result.append(WeightedX(1, row))
     return result
 
+def getBankAttributes() :
+    bank_attributes = {
+        "age": "numeric",
+        "job": ["admin.", "unknown", "unemployed", "management", "housemaid", "entrepreneur", "student", "blue-collar", "self-employed", "retired", "technician", "services"],
+        "marital": ["married", "divorced", "single"],
+        "education": ["unknown", "secondary", "primary", "tertiary"],
+        "default": ["yes", "no"],
+        "balance": "numeric",
+        "housing": ["yes", "no"],
+        "loan": ["yes", "no"],
+        "contact": ["unknown", "telephone", "cellular"],
+        "day": "numeric",
+        "month": ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
+        "duration": "numeric",
+        "campaign": "numeric",
+        "pdays": "numeric",
+        "previous": "numeric",
+        "poutcome": ["unknown", "other", "failure", "success"]
+    }
+    return bank_attributes
+
 def adaBoost():
-    data = saveDataSet('../datasets/bank/train.csv')
-    print(data[0].weight, data[0].data)
+    dataSet = saveDataSet('../datasets/bank/train.csv')
+    d3 = DecisionTree(dataSet=dataSet, attributesWithValues=getBankAttributes(), hasNumerics=True, max_depth=2, replaceMissing=True)
+    print("uh")
+    d3.printTree(d3.root, "")
 
 if __name__ == '__main__':
     adaBoost()
