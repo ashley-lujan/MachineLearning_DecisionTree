476aca8a7cb966a71c0b539ba6d0f2cbf507518e
Ashley Lujan <lujanashlet@gmail.com>
2024-11-25 21:50:12 -0700

    svm stochastic

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,38 +1,93 @@
 import numpy as np
 
 def stochastic_svm(training_data, tune_rate, C, epochs):
+    #let w be w with bias vector
     d = train_data.shape[1]
     n = train_data.shape[0]
-    w = np.zeros(d)
+    w = np.zeros(d - 1) 
+    # w0 = np.zeros(d - 2)
     for t in range(epochs): 
         train_x, train_y = shuffle(training_data)
         for xi, yi in zip(train_x, train_y):
             if yi * (w @ xi) <= 1: 
-                #what the heck is [w0; 0]
-                w = w - (yi * (tune_rate(t) * w)) + tune_rate(t) * C * n * yi * xi
+                w0 = np.insert(w[1:], 0, 0)
+                w = w - (yi * (tune_rate(t) * w0)) + tune_rate(t) * C * n * yi * xi
             else: 
                 w = (1 - tune_rate(t)) * w
     return w
 
 def shuffle(train_data): 
-    d = train_data.shape[1]
     np.random.shuffle(train_data)
-    train_x = train_data[:, :(d-1)]
-    train_y = train_data[:, (d - 1)]
-    return train_x, train_y
+    return seperate_data(train_data)
+
+def seperate_data(data): 
+    d = data.shape[1]
+    x = data[:, :(d-1)]
+    y = data[:, (d - 1)]
+    return x, y
+
+def report_results(w, data, d_type): 
+    x, y = seperate_data(data)
+    print("The weight vector is {} with accuracy of {} on {} data".format(w, evaluate_w(w, x, y), d_type))
+
+def evaluate_w(w, test_x, test_y): 
+    correct = 0
+    for xi, yi in zip(test_x, test_y): 
+        if yi == predict(xi, w): 
+            correct += 1
+    return correct / (test_x).shape[0]
+
+def predict(xi, w): 
+    yi = w @ xi
+    if yi > 0: 
+        return 1
+    return -1
+
+def attach_ones(data): 
+    ones_column = np.ones((data.shape[0], 1))
+    return np.hstack((ones_column, data))
+
+def learning_rate_a(t, y0 = 0.1, a = 1):
+    return (y0)/(1 + (y0/a) * t)
+
+def learning_rate_b(t, y0 = 0.1):
+    return (y0)/(1 + t)
+
+
+
 
 if __name__ == "__main__":
     #use rate, use different c's
+
     train_data_filename = "datasets/bank-note/train.csv"
     test_data_filename = "datasets/bank-note/test.csv"
 
     test_data = np.genfromtxt(test_data_filename, delimiter=',', dtype=float) 
     train_data = np.genfromtxt(train_data_filename, delimiter=',', dtype=float) 
-
     d = train_data.shape[1]
 
+    #rewrite 0
     train_data[:, d - 1] = np.where(train_data[:, d - 1] == 0, -1, train_data[:, d-1])
+    test_data[:, d - 1] = np.where(test_data[:, d - 1] == 0, -1, test_data[:, d-1])
+
+    #SVM Experiment 
+    #append ones to data
+    train_data = attach_ones(train_data)
+    test_data = attach_ones(test_data)
+
     C = [100/873, 500/873, 700/873]
-    def learning_rate_a(t, y0 = 1, a = 1):
-        return (y0)/(1 + (y0/a) * t)
-    stochastic_svm(train_data, learning_rate_a, C[0], 2)
+    learning_rates = [learning_rate_a, learning_rate_b]
+    for learn_rate in learning_rates: 
+        print("question")
+        for c in C: 
+            print("C = {}".format(c))
+            w = stochastic_svm(train_data, learn_rate, C[0], 100)
+            # report_results(w, train_data, "train")
+            report_results(w, test_data, "test")
+
+
+    
+
+
+
+    
